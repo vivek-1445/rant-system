8434d0bbe7da38ccb2b67a07856901179da371e5
vivek-1445 <58764111+vivek-1445@users.noreply.github.com>
2020-03-27 18:45:10 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,8 @@
 
 <?php
 
-$database_connection = mysqli_connect("localhost","root","","rent-system")or die("database does not connect");
+$database_connection = mysqli_connect("us-cdbr-iron-east-01.cleardb.net","bcbbd0365fdf3d","2e25b24d","heroku_70dfe69cb687717")or die("database does not connect");
 
 ?>
 
+database changes
